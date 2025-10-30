054ebf83813bb8a85f95dc025ce1e05c2443cb0d
Jackson Strazzo <jstrazzo22@gmail.com>
2025-10-30 18:25:12 +0000

    Completed HW4 - DATA 3500

--- HW4/example.py
+++ HW4/example.py
@@ -1,4 +1,4 @@
-# This is so I can understand how to loop through this bc
+# This is so I can understand how to loop through this bc I'm super duper confused
 
 prices = [10, 12, 11, 13, 15, 14, 16, 17, 18, 20]  # example list
 
@@ -9,3 +9,6 @@ for i in range(len(prices) - 4):
 for i in range(len(prices) - 4):
     five_day_avg = sum(prices[i:i+5]) / 5
     current_price = prices[i + 4]
+
+
+# CHATGPT CAME UP W/ THIS EXAMPLE FYI
